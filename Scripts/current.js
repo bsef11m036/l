a21130729e2992a3eaa327c7c66633dcb6585107
bsef11m036 <bsef11m036@pucit.edu.pk>
2015-02-10 18:52:43 +0500

    Commit

---
+++ Scripts/current.js
@@ -0,0 +1,24 @@
+﻿$().ready(function () {
+
+    $("#TName").autocomplete(tags, {
+        mustMatch: true,
+        autoFill: true,
+        multiple: true
+    });
+    $("#TName1").autocomplete(tags, {
+        mustMatch: true,
+        autoFill: true,
+        multiple: true
+    });
+    $("#TName2").autocomplete(tags, {
+        mustMatch: true,
+        autoFill: true,
+        multiple: true
+    });
+    $("#TName3").autocomplete(tags, {
+        mustMatch: true,
+        autoFill: true,
+        multiple: true
+    });
+});
+
---
+++ Scripts/localdata.js
@@ -0,0 +1,39 @@
+
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var emails = [
+	{ name: "Peter Pan", to: "peter@pan.de" },
+	{ name: "Molly", to: "molly@yahoo.com" },
+	{ name: "Forneria Marconi", to: "live@japan.jp" },
+	{ name: "Master <em>Sync</em>", to: "205bw@samsung.com" },
+	{ name: "Dr. <strong>Tech</strong> de Log", to: "g15@logitech.com" },
+	{ name: "Don Corleone", to: "don@vegas.com" },
+	{ name: "Mc Chick", to: "info@donalds.org" },
+	{ name: "Donnie Darko", to: "dd@timeshift.info" },
+	{ name: "Quake The Net", to: "webmaster@quakenet.org" },
+	{ name: "Dr. Write", to: "write@writable.com" }
+];
+var tags = [
+	"c++", "c#", "c#.net", "asp.net", "mvc", "mvc-4", "oracle", "sql",
+	"mysql", "java", "javascript", "c", "php", "phython", "ruby",
+	"smtp", "int", "float", "html", "css", "android", "ios",
+	"java beans", "abstract", "jquery", "json", "ojective-c", ".net", "sql-server",
+	"xml", "linux", "database", "eclipse", "ajax", "string", "html-5",
+	"oracle", "visual studio", "web-services", "css3", "wcf", "perl", "matlab", "net-beans",
+	"entity framwork", "linq", "ado.net", "oop", "http", "listview",
+	"debugging", "excel", "sorting", "loops", "silverlight", "azure", "ms-access",
+	"unix", "svn", "animation", "dom", "ubuntu", "button",
+	"textbox", "textarea", "post", "get", "networking", "selenium",
+	"oject"];
+var cities = [
+	"c++", "c#", "c#.net", "asp.net", "mvc", "mvc-4", "oracle", "sql",
+	"mysql", "java", "javascript", "c", "php", "phython", "ruby",
+	"smtp", "int", "float", "html", "css", "android", "ios",
+	"java beans", "abstract", "jquery", "json", "ojective-c", ".net", "sql-server",
+	"xml", "linux", "database", "eclipse", "ajax", "string", "html-5",
+	"oracle", "visual studio", "web-services", "css3", "wcf", "perl", "matlab", "net-beans",
+	"entity framwork", "linq", "ado.net", "oop", "http", "listview",
+	"debugging", "excel", "sorting", "loops", "silverlight", "azure", "ms-access",
+	"unix", "svn", "animation", "dom", "ubuntu", "button",
+	"textbox", "textarea", "post", "get", "networking", "selenium",
+	"oject"];
